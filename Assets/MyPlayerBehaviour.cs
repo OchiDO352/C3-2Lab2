b10640bc495dc1e4188cc48c880d257fc6e7599a
OchiDO352 <75597102+OchiDO352@users.noreply.github.com>
2021-12-26 16:32:26 +0700

    Update MyPlayerBehaviour.cs

--- Assets/MyPlayerBehaviour.cs
+++ Assets/MyPlayerBehaviour.cs
@@ -1,55 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class MyPlayerBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public float speed;
-    public WeaponBehaviour[] weapons;
+   public float speed;
+    public WeaponBehaviour [] weapons;
     public int arraySize;
     int selectedWeaponIndex = 0;
-    void Start()
-    {
-        References.thePlayer = gameObject;
-        weapons = new WeaponBehaviour[1];
-        for (int index = 0; index < arraySize; index++)
-        {
-            weapons[index] = new WeaponBehaviour();
-        }
-        selectedWeaponIndex = 0;
+void Start()
+{
+    References.thePlayer = gameObject;
+    weapons = new WeaponBehaviour [arraySize];
+    for (int index =0;index<arraySize;index++){
+        weapons[index] = new WeaponBehaviour();
     }
-    // Update is called once per frame
-    void Update()
+selectedWeaponIndex = 0;
+}
+// Update is called once per frame
+void Update()
+{
+//การเคลื่อนที่ของแท่นปืน
+    Vector3 inputVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    Rigidbody ourRigidBody = GetComponent<Rigidbody>();
+    ourRigidBody.velocity = inputVector * speed;
+//การหมุนแท่นปืนเป็นวงกลม
+    Ray rayFromCameraToCursor = Camera.main.ScreenPointToRay(Input.mousePosition);
+    Plane playerPlane = new Plane(Vector3.up, transform.position);
+    playerPlane.Raycast(rayFromCameraToCursor, out float distanceFromCamera);
+    Vector3 cursorPosition = rayFromCameraToCursor.GetPoint(distanceFromCamera);
+    gameObject.transform.LookAt(cursorPosition);
+//กำหนดให้กดปุ่มเม้าซ้ายยิงปืน
+// if (weapons.Count > 0 && Input.GetButton("Fire1"))
+// {
+// //Tell our weapon to fire
+// weapons[selectedWeaponIndex].Fire(cursorPosition);
+// }
+    if ( weapons.Length >0 && Input.GetButton("Fire1"))
     {
-        //การเคลื่อนที่ของแท่นปืน
-        Vector3 inputVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        Rigidbody ourRigidBody = GetComponent<Rigidbody>();
-        ourRigidBody.velocity = inputVector * speed;
-        //การหมุนแท่นปืนเป็นวงกลม
-        Ray rayFromCameraToCursor = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Plane playerPlane = new Plane(Vector3.up, transform.position);
-        playerPlane.Raycast(rayFromCameraToCursor, out float distanceFromCamera);
-        Vector3 cursorPosition = rayFromCameraToCursor.GetPoint(distanceFromCamera);
-        gameObject.transform.LookAt(cursorPosition);
-        //กำหนดให้กดปุ่มเม้าซ้ายยิงปืน
-        // if (weapons.Count > 0 && Input.GetButton("Fire1"))
-        // {
-        // //Tell our weapon to fire
-        // weapons[selectedWeaponIndex].Fire(cursorPosition);
-        // }
-        if (Input.GetButton("Fire1"))
-        {
-            //Tell our weapon to fire
-            weapons[selectedWeaponIndex].Fire(cursorPosition);
-        }
-        //ทำการเปลี่ยนอาวุธกดปุ่มเมาขวา
-        if (weapons.Length > 0 && Input.GetButtonDown("Fire2"))
-        {
-            ChangeWeaponIndex(selectedWeaponIndex + 1);
-        }
+//Tell our weapon to fire
+    weapons[selectedWeaponIndex].Fire(cursorPosition);
     }
 
-    //ทำการเปลี่ยนอาวุธ
+    //ทำการเปลี่ยนอาวุธกดปุ่มเมาขวา
+    if (weapons.Length >0 && Input.GetButtonDown("Fire2"))
+    {
+            ChangeWeaponIndex(selectedWeaponIndex + 1);
+    }
+}
+   //ทำการเปลี่ยนอาวุธ
     private void ChangeWeaponIndex(int index)
     {
 
@@ -72,8 +72,7 @@ public class MyPlayerBehaviour : MonoBehaviour
             {
                 //If it's the one we just selected, make it visible - 'enable' it
                 weapons[i].gameObject.SetActive(true);
-            }
-            else
+            } else
             {
                 //If it's not the one we just selected, hide it - disable it.
                 weapons[i].gameObject.SetActive(false);
@@ -83,18 +82,18 @@ public class MyPlayerBehaviour : MonoBehaviour
 
     //เมื่อแท่นปืนชนเข้ากับอาวุธ
     private void OnTriggerEnter(Collider other)
+{
+WeaponBehaviour theirWeapon = other.GetComponentInParent<WeaponBehaviour>();
+    if (theirWeapon != null)
     {
-        WeaponBehaviour theirWeapon = other.GetComponentInParent<WeaponBehaviour>();
-        if (theirWeapon != null)
-        {
-            //เพื่ออาวุธ
-            //weapons.Add(theirWeapon);
-            weapons[selectedWeaponIndex] = theirWeapon;
-            //เปลี่ยนตำแหน่ง
-            theirWeapon.transform.position = transform.position;
-            theirWeapon.transform.rotation = transform.rotation;
-            //เปลี่ยนให้อาวุธเป็นผู้ติดตาม
-            theirWeapon.transform.SetParent(transform);
-        }
+//เพื่ออาวุธ
+//weapons.Add(theirWeapon);
+    weapons[selectedWeaponIndex] = theirWeapon;
+//เปลี่ยนตำแหน่ง
+    theirWeapon.transform.position = transform.position;
+    theirWeapon.transform.rotation = transform.rotation;
+//เปลี่ยนให้อาวุธเป็นผู้ติดตาม
+    theirWeapon.transform.SetParent(transform);
     }
+ }
 }
